ecb42cd1b07e4f32ed2919b0ba1e62257873c99d
Will Smith <wsmith@Wills-MacBook-Pro.local>
2017-11-23 20:20:43 -0700

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("user flipped " + cardFour);
+console.log("user flipped " + cardOne);
