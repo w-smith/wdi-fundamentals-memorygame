aefcc6457ffa2cee105928216ba259e3db9c1a46
Will Smith <wsmith@Wills-MacBook-Pro.local>
2017-11-25 22:59:43 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("user flipped " + cardFour);
-console.log("user flipped " + cardOne);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push("cardOne");
+
+console.log("user flipped queen");
+
+var cardTwo = cards[2];
+
+cardsInPlay.push("cardTwo");
+
+console.log("user flipped king");
+
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  	alert("You found a match!");
+  } else { 
+  	alert("Sorry, try again."); 
+  }
+
+  }
+
+
