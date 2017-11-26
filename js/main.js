9d4f141db870dd1051a10d3027f87481c92dc77b
Will Smith <wsmith@Wills-MacBook-Pro.local>
2017-11-26 14:26:56 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,27 +1,53 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'image/queen-of-hearts.png'
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+}
+];
 
 var cardsInPlay = [];
-cardsInPlay.push("cards[cardId]");
+cardsInPlay.push("cards[cardId].rank");
+
 
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  	console.log("You found a match!");
+  	alert("You found a match!");
   } else { 
-  	console.log("Sorry, try again."); }
+  	alert("Sorry, try again."); }
 }
 
 
 var flipCard = function(cardId) {
-console.log("User flipped " + cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
 checkForMatch();
 if (cardsInPlay.length === 2) {
   }
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+
 }
 
-flipCard(0);
+flipCard(0)
+flipCard(2)
 
-flipCard(2);
+
 
